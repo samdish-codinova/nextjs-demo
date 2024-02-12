6e34effe62f8691e30050267b5319447f0cddeb1
Samdish <samdish.singh@codinova.com>
2024-02-12 19:21:05 +0530

    Integrate delete article mutation

--- src/app/articles/[id]/DeleteArticleButton.tsx
+++ src/app/articles/[id]/DeleteArticleButton.tsx
@@ -2,11 +2,28 @@
 
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import Button from "@mui/material/Button";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
+const mutation = `
+  mutation DeleteArticle($id: String!) {
+  deleteArticle(id: $id) {
+    id
+  }
+}
+`;
+
+type DeleteArticleResponse = {
+  data?: {
+    deleteArticle: { id: string };
+  } | null;
+  errors?: [{ message: string; [x: string]: unknown }];
+};
+
 const DeleteArticleButton = ({ articleId }: { articleId: string }) => {
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const handleDelete = async () => {
     setLoading(true);
@@ -14,9 +31,36 @@ const DeleteArticleButton = ({ articleId }: { articleId: string }) => {
     try {
       console.log("Deleting article with id ", articleId);
 
-      toast.success("Article has been deleted");
+      const deleteResponse: DeleteArticleResponse = await fetch(
+        "http://localhost:5000/graphql",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            query: mutation,
+            variables: { id: articleId },
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      ).then((res) => res.json());
+
+      const errors = deleteResponse?.errors;
+      const deletedArticleId = deleteResponse?.data?.deleteArticle?.id;
+
+      if (deletedArticleId) {
+        toast.success("Article has been deleted");
+        router.replace("/articles");
+      } else if (errors) {
+        toast.error(
+          errors?.[0]?.message ||
+            "Could not delete article. Please try again later"
+        );
+      }
     } catch (error) {
-      return toast.error("Could not delete article please try again later!");
+      return toast.error(
+        error?.toString() || "Could not delete article please try again later!"
+      );
     } finally {
       setLoading(false);
     }
